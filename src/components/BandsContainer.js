9b4acc9d094f78b10c07fc6e2ff5c63d8a698884
Dani Lyn <dlyn1110@gmail.com>
2019-06-24 14:59:55 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -20,8 +20,8 @@ class BandsContainer extends Component {
 const mapStateToProps = ({ bands }) => ({ bands })
 
 const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
-  deleteBand: id => dispatch({ type: "DELETE_BAND", id})
+  addBand: name => dispatch({ type: "ADD_BAND", name: name })
+  deleteBand: id => dispatch({ type: "DELETE_BAND", id: id})
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
