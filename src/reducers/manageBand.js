b0c1fa51cff82ab68a36f224002536c5ee1ff8ce
Dani Lyn <dlyn1110@gmail.com>
2019-06-24 15:11:08 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,6 +3,10 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+    let band = {
+      name: action.name,
+      id: Math.random()*1000000000
+    }
 
       return { ...state, bands: [...state.bands, action.name] }
 
