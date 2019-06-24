b6f727105c2fffcb8a30b8c0adbad51e27a81445
Dani Lyn <dlyn1110@gmail.com>
2019-06-24 15:12:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -8,7 +8,10 @@ export default function manageBand(state = {
       id: Math.random()*1000000000
     }
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return { ...state, bands: [...state.bands, band }
+
+        case 'DELETE_BAND':
+        let bands = state.bands.filter(band => band.id !==action.id)
 
     default:
       return state;
